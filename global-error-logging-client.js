a69f64cccb71b1734a72f14369d8f450ef5ed06a
Guilherme Decampo <guilherme.decampo@gmail.com>
2016-01-06 22:00:51 -0200

    Add hijack on Meteor._debug and Meteor.startup

--- global-error-logging-client.js
+++ global-error-logging-client.js
@@ -1,5 +1,6 @@
 let trackGlobalErrors = function() {
   if (!Meteor.settings.public.slack ||
+      !Meteor.settings.public.slack.isProd ||
       !Meteor.settings.public.slack.trackGlobalErrors) {
     return false;
   }
@@ -11,6 +12,18 @@ let trackGlobalErrors = function() {
   window.addEventListener('error', function(error) {
     slackLog.error(`${error.message}, \n${error.filename}: ${error.lineno}`);
   });
+
+  /**
+   * Borrowed the idea of hijack Meteor._debug from kadira
+   * https://github.com/meteorhacks/kadira/blob/59b1b27035ad4a2ad5a911fad37ec8eb8b7aed40/lib/hijack/error.js
+   */
+  let originalMeteorDebug = Meteor._debug;
+  Meteor._debug = function (message, stack) {
+    slackLog.error(`${message}, \n${stack}`, 'server');
+    return originalMeteorDebug.apply(this, arguments);
+  };
 };
 
-trackGlobalErrors();
+Meteor.startup(function () {
+  trackGlobalErrors();
+});
